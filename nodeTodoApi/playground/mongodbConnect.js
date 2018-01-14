5de544e52dd60518a4fa6ff16bfed974037a8eaf
sj70 <shubhamjangid70@gmail.com>
2018-01-14 18:52:50 +0530

    practicing

--- nodeTodoApi/playground/mongodbConnect.js
+++ nodeTodoApi/playground/mongodbConnect.js
@@ -1,32 +1,70 @@
-const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient;
 
-// const {MongoClient}=require('mongodb');
+// // const {MongoClient}=require('mongodb');
 
-// var person={
-//   name:"shubham",
-//   age:20
-// }
+// // var person={
+// //   name:"shubham",
+// //   age:20
+// // }
 
-// var {age}=person;
-// console.log(age);
+// // var {age}=person;
+// // console.log(age);
 
-MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+// MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+//   if (err) {
+//     return console.log("Unable to connect to MongoDB server");
+//   }
+//   console.log("Connected to MongoDB server");
+
+//   db.collection("Todos").insertOne(
+//     {
+//       text: "Something to do",
+//       completed: true
+//     },
+//     (err, result) => {
+//       if (err) {
+//         return console.log("Unable to insert todo", err);
+//       }
+
+//       console.log(JSON.stringify(result.ops[0].text));
+//     }
+//   );
+
+//   db.close();
+// });
+
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
-    return console.log("Unable to connect to MongoDB server");
+    return console.log('cannot connect to mongodb server');
   }
-  console.log("Connected to MongoDB server");
+  console.log('connected to mongodb server');
 
-  db.collection("Todos").insertOne(
+  // db.collection('Todos').insertOne(
+  //   {
+  //     text: 'something to do',
+  //     completed: false
+  //   },
+  //   (err, result) => {
+  //     if (err) {
+  //       return console.log('cannot add data');
+  //     }
+  //     console.log(JSON.stringify(result.ops, undefined, 2));
+  //   }
+  // );
+
+  db.collection('Users').insertOne(
     {
-      text: "Something to do",
-      completed: true
+      name: 'shubham jangid',
+      age: 20,
+      address: ' new delhi india'
     },
     (err, result) => {
       if (err) {
-        return console.log("Unable to insert todo", err);
+        return console.log('cannot add data ');
       }
-
-      console.log(JSON.stringify(result.ops[0].text));
+      console.log(JSON.stringify(result.ops, undefined, 2));
     }
   );
 
--- nodeTodoApi/playground/mongodbFind.js
+++ nodeTodoApi/playground/mongodbFind.js
@@ -1,30 +1,53 @@
-const MongoClient = require("mongodb").MongoClient;
-MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
-  if (err) {
-    return console.log("cannot connect to mongodb server");
-  }
-  console.log("connection made");
+// const MongoClient = require("mongodb").MongoClient;
+// MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+//   if (err) {
+//     return console.log("cannot connect to mongodb server");
+//   }
+//   console.log("connection made");
 
-  // db.collection('Todos').find({completed:true}).toArray().then((docs)=>{
-  //     console.log("todos");
-  //     // console.log(`todos  ${JSON.stringify(docs)`);
-  //     console.log(JSON.stringify(docs,undefined , 2));
-  // },(err)=>{
-  //     console.log("error occured");
-  // }
-  // );
+//   // db.collection('Todos').find({completed:true}).toArray().then((docs)=>{
+//   //     console.log("todos");
+//   //     // console.log(`todos  ${JSON.stringify(docs)`);
+//   //     console.log(JSON.stringify(docs,undefined , 2));
+//   // },(err)=>{
+//   //     console.log("error occured");
+//   // }
+//   // );
+//   db
+//     .collection("Todos")
+//     .find({ text: "Something to do" })
+//     .toArray()
+//     .then(
+//       docs => {
+//         console.log(` Todos count ${JSON.stringify(docs, undefined, 2)}`);
+//       },
+//       err0 => {
+//         console.log("error occured");
+//       }
+//     );
+
+//   db.close();
+// });
+
+const { MongoClient } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('cannot connect to mongodb server');
+  }
+  console.log('connected to server');
+
   db
-    .collection("Todos")
-    .find({ text: "Something to do" })
+    .collection('Todos')
+    .find()
     .toArray()
     .then(
       docs => {
-        console.log(` Todos count ${JSON.stringify(docs, undefined, 2)}`);
+        console.log(`data is ${JSON.stringify(docs, undefined, 2)}`);
       },
-      err0 => {
-        console.log("error occured");
+      err => {
+        console.log('cannot find the data');
       }
     );
-
   db.close();
 });

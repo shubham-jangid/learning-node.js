e59c46d405a7cfb537159b8992eaadfbcbc434f7
user <shubhamjangid70@gmail.com>
2018-01-23 14:19:32 +0530

    some init msg

--- nodeTodoApi/server/models/user.js
+++ nodeTodoApi/server/models/user.js
@@ -1,11 +1,36 @@
 const mongoose = require('mongoose');
-var Users = mongoose.model('Users', {
+const validator = require('validator');
+
+var User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      // or i can use validator:validator.isEmail
+      validator: validator.isEmail,
+      message: ' { value }  is not valid Email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
-module.exports = { Users };
+module.exports = { User };
--- nodeTodoApi/server/server.js
+++ nodeTodoApi/server/server.js
@@ -1,10 +1,12 @@
 //importing the library modules
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
+var _ = require('lodash');
 
 // importing the local modules
 var { mongoose } = require('./db/mongoose');
-var { Users } = require('./models/user');
+var { User } = require('./models/user');
 var { Todo } = require('./models/Todo');
 
 var app = express();
@@ -29,10 +31,10 @@ app.post('/todos', (req, res) => {
 // ------------ Get request -------------------
 
 app.get('/todos', (req, res) => {
-  Todo.findById({ _id: '5a5f91b144fcbbd41763a7dc' }).then(
-    docs => {
-      res.send(docs);
-      if (!docs) {
+  Todo.find().then(
+    todos => {
+      res.send(todos);
+      if (!todos) {
         return console.logs('cannot find docs');
       }
     },
@@ -41,6 +43,90 @@ app.get('/todos', (req, res) => {
     }
   );
 });
+
+//--------- geting request on particular id ---------
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    console.log('Invalid ID');
+    return res.status(404).send('invalid ID');
+  }
+  Todo.findById(id).then(todo => {
+    if (!todo) {
+      console.log('todo empty');
+      return res.status(404).send('todo empty');
+    }
+    res.send(todo);
+  });
+});
+
+//--------- todo delete ---------
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send('todo is emypty');
+      }
+      res.send(todo);
+    })
+    .catch(err => {
+      res.status(404).send();
+    });
+});
+
+//-------updating the todo---------
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    console.log('Invalid ID');
+    return res.status(404).send('invalid ID');
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(err => {
+      return res.status(404).send();
+    });
+});
+
+//-------- post users-----------
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  user
+    .save()
+    .then(user => {
+      res.send(user);
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(404).send(err);
+    });
+});
+
+//-------listening to port--------
 
 app.listen(3000, () => {
   console.log('on port 3000');
